f455c5171f9a2dd0cce2250c9f9bd7aa6c8180cb
DiogomCunha <ddmoraes.ddm@gmail.com>
2022-06-21 13:57:48 -0300

    Diogo

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,17 @@
-let btn = document.querySelector(".buttonAcessar")
+let btn = document.querySelector(".buttonAcessar");
+let lab = document.querySelector("label");
+let email = document.getElementById("inputEmail");
+let senha = document.getElementById("inputPassword");
 
-let email = document.getElementById ("inputEmail")
-let senha = document.getElementById ("inputPassword")
-
-function inputEntries(){
-
-    if (email.value !="" && senha.value != "")
+function inputEntries() {
+  if (email.value != "" && senha.value != "") {
+    btn.style.backgroundColor = "#7898FF";
     btn.disabled = false;
-    
-    else
+  } else if (email.value == "" || senha.value == "") {
+    btn.style.backgroundColor = "#D3D3D3"
     btn.disabled = true;
-
-    
+  }
 }
 
-function btnDisabled(){
-
-    btn.disbled = true;
-
-}
+email.value = email.value.trim();
+senha.value = senha.value.trim();
