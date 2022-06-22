083bb0828c45d769c85d3016f46d1b5e1f320f34
ErikSD2 <you@example.com>
2022-06-21 22:49:06 -0300

    Regex

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,26 @@
+const mascaraEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+const mascaraPwd = /.{4,}/g
 let btn = document.querySelector(".buttonAcessar");
 let lab = document.querySelector("label");
 let email = document.getElementById("inputEmail");
 let senha = document.getElementById("inputPassword");
 
 function inputEntries() {
-  if (email.value != "" && senha.value != "") {
+  if (ValidarInput(email, mascaraEmail) && ValidarInput(senha, mascaraPwd)) {
     btn.style.backgroundColor = "#7898FF";
     btn.disabled = false;
-  } else if (email.value == "" || senha.value == "") {
+  } else {
     btn.style.backgroundColor = "#D3D3D3"
     btn.disabled = true;
   }
 }
 
-email.value = email.value.trim();
-senha.value = senha.value.trim();
+function ValidarInput(texto, mascara) {
+  var resultado = texto.value.match(mascara);
+  if (resultado != null) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
