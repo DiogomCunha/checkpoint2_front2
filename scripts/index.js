c59a34942a4cae69ef60c862518c50eb477e2fb2
ErikSD2 <you@example.com>
2022-06-29 22:42:02 -0300

    correcao1

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,32 @@ let btn = document.querySelector(".buttonAcessar");
 let lab = document.querySelector("label");
 let email = document.getElementById("inputEmail");
 let senha = document.getElementById("inputPassword");
+const urlApi = "https://https://ctd-todo-api.herokuapp.com/v1/users/login";
 
-function inputEntries() {
+function validacaoLogin() {
   if (ValidarInput(email, mascaraEmail) && ValidarInput(senha, mascaraPwd)) {
     btn.style.backgroundColor = "#7898FF";
     btn.disabled = false;
   } else {
-    btn.style.backgroundColor = "#D3D3D3"
+    btn.style.backgroundColor = "#D3D3D3";
     btn.disabled = true;
   }
-};
+}
 
+function inputEntries() {
+  const data = {
+    email: emailSign.value,
+    password: senhaSign.value,
+  };
+  const configRequis = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  fetch(urlApi, configRequis)
+      .then((res) => {
+        return res.json();
+      })
+}
--- scripts/signup.js
+++ scripts/signup.js
@@ -5,79 +5,108 @@ let sobrenomeSign = document.getElementById("inputSignSobrenome");
 let emailSign = document.getElementById("inputSignMail");
 let senhaSign = document.getElementById("inputSignPassword");
 let senhaSignConf = document.getElementById("inputSignPasswordConfirm");
-const urlApi = 'https://ctd-todo-api.herokuapp.com/v1/users';
+const urlApi = "https://ctd-todo-api.herokuapp.com/v1/users";
 
+btn.addEventListener("click", (evento) => {
+  evento.preventDefault();
 
+ 
+});
 
-function inputEntries() {
+emailSign.addEventListener('change',validacao)
+nomeSign.addEventListener('change',validacao)
+sobrenomeSign.addEventListener('change',validacao)
+senhaSign.addEventListener('change',validacao)
+senhaSignConf.addEventListener('change',validacao)
+
+function validacao(){
   if (
-    
     ValidarInput(nomeSign, mascaraNome) &&
     ValidarInput(sobrenomeSign, mascaraSobrenome) &&
     ValidarInput(emailSign, mascaraEmail) &&
     ValidarInput(senhaSign, mascaraPwd) &&
-    ValidarInput(senhaSignConf, mascaraPwd) &&
-    senhaSign === senhaSignConf
+    ValidarInput(senhaSignConf, mascaraPwd) 
+    && senhaSign.value === senhaSignConf.value
+  ){btn.style.backgroundColor = "#7898FF";
+  btn.disabled = false;}
+  
+  else {
+    btn.style.backgroundColor = "#D3D3D3";
+    btn.disabled = true;}
+};
 
-  ) {
-    btn.style.backgroundColor = "#7898FF";
-    btn.disabled = false;
+
+
+function inputEntries() {
+   
+    
 
     const data = {
-
       firstName: nomeSign.value,
       lastName: sobrenomeSign.value,
       email: emailSign.value,
-      password: senhaSign.value
-  }
+      password: senhaSign.value,
+    };
     const configRequis = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     };
     fetch(urlApi, configRequis)
+      .then((res) => {
+        return res.json();
+      })
 
-    .then((res)=>{
+      .then(function (resp) {
+        console.log(resp);
 
-      if (res.status == 201){
-        return res.json()
-      }
+        sucessoCadastro(
+          nomeSign.value,
+          sobrenomeSign.value,
+          emailSign.value,
+          resp
+        );
+      })
+
+      .catch((error) => {
+        cadastroErro(error);
+      });
+  } 
+    //alert('Todos os campos devem estar preenchidos corretamente para continuar!')
+  
+
+function sucessoCadastro(nomeSign, sobrenomeSign, emailSign, jwtRecebido) {
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      nomeSign: nomeSign,
+      sobrenomeSign: sobrenomeSign,
+      emailSign: emailSign,
+      token: jwtRecebido,
     })
+  );
 
-    .then(function(resp){
-      console.log(resp)
+  alert("Usuário criado com sucesso!");
+}
 
-      sucessoCadastro(nomeSign.value, sobrenomeSign.value, emailSign.value, resp.jwt)
-    })
+function cadastroErro(statusErro) {
+  console.log("Erro ao cadastrar usuário");
+  console.log(statusErro);
+}
 
-    .catch(error => {
-        cadastroErro(error)
-    })
-  } else {
-
-    alert('Todos os campos devem estar preenchidos corretamente para continuar!')
-    btn.style.backgroundColor = "#D3D3D3";
-    btn.disabled = true;
-  }
-
-};
-
-function sucessoCadastro(nomeSign, sobrenomeSign, emailSign,)
-
-//passo 1: 
+//passo 1:
 //desenvolver o corpo da requisição
 //método
 //cabeçalho
 //corpo
 
-//passo 2: 
+//passo 2:
 //efetuar o fetch da API
 //tratar no segundo then a execução da função
 
-//passo 3: 
+//passo 3:
 //criar a função de cadastro
 //armazenar no local storage o nome, sobrenome e e-mail
 //armazenar utilizando objeto json (json stringfy)
-
